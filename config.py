4d10b45e70836dbcb6e0a7bf0efd37179e3b3d64
cluelss <108840531+cluelss@users.noreply.github.com>
2022-07-06 19:44:08 -0400

    Update config.py

--- config.py
+++ config.py
@@ -7,16 +7,13 @@ STARTING_HAND_SIZE = 6
 PACK_PRICE = 100
 CHALLENGE_TIMEOUT = 30 #How long to wait for someone to accept a challenge
 TURN_TIMEOUT = 300 #How long to wait for someone to do an action on their turn before they forfeit the match
-TOKEN = '' #secret
+TOKEN = 'OTk0Mzg2NzU0NjQ1Nzk0OTA2.GbtxtS.6KYHFoeG83OjU3au0Spe4nqqhg5wgQonLh5yfI' 
 
 DEFINITIONS = {
-	"lifeforce": "Your primary resource. You use this to pay card and Node costs. If it reaches 0, you lose.",
-	"node": "An object that stays on the board. They can have spawn abilities, death abilities, and abilities that activate on each of your turns.",
-	"mill": "Removes the top card of your deck from the game, and you gain lifeforce equal to its cost.",
-	"burn": "Removes the top card of your deck from the game. You do not gain lifeforce.",
-	"energy": "You gain lifeforce equal to your Energy at the start of each of your turns.",
-	"hunger": "Affects how much lifeforce you gain from sacrificing Nodes. You start with 10, and can go down to 0.",
-	"desperation": "Affects how much lifeforce you gain from milling. You start with 10, and can go down to 0.",
+	"trade offer": "An offer to exchange different cards/characters for another card/character.",
+	"wishlist": "Player's most wanted cards.",
+	"ownings": "Each player's current owned characters.",
+	"give away": "Removes card from your owned characters.",
 }
 
 #don't touch
